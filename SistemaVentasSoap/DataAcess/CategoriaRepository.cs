4c0c2f5b52e6bc30d08178037159f0c744ad706c
Julian3370 <135557819+Julian3370@users.noreply.github.com>
2023-08-25 14:56:18 -0500

    Se agrega Listar Productos por categoria

---
+++ SistemaVentasSoap/DataAcess/CategoriaRepository.cs
@@ -0,0 +1,100 @@
+﻿using SistemaVentasSoap.DataAcess.Interfaces;
+using SistemaVentasSoap.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Windows.Input;
+
+namespace SistemaVentasSoap.DataAcess
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        public List<Categoria> GetAll()
+        {
+            List<Categoria> categorias = new List<Categoria>();
+            try
+            {
+                using (SqlConnection connection = new DbContext().GetConnection())
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("select * from CATEGORIA", connection);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Categoria categoria = new Categoria
+                            {
+                                Id = (int)reader["Id"],
+                                Descripcion = (string)reader["Descripcion"]
+                            };
+                            categorias.Add(categoria);
+                        }
+                    }
+                }
+                return categorias;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+        public string AgregarCategoria(Categoria categoria)
+        {
+            try
+            {
+                using (SqlConnection connection = new DbContext().GetConnection())
+                {
+                    connection.Open();
+                    string query = "INSERT INTO Categoria (Descripcion) VALUES (@Descripcion)";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Descripcion", categoria.Descripcion);
+                        int rowsAffected = command.ExecuteNonQuery();
+                        return "Categoria Creada";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+        public List<Producto> ListarPorCategorias(int IdCategoria)
+        {
+            List<Producto> pcategorias = new List<Producto>();
+            try
+            {
+                using (SqlConnection connection = new DbContext().GetConnection())
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("select * from PRODUCTO where idCategoria = @IdCategoria;", connection);
+                    command.Parameters.AddWithValue("@IdCategoria", IdCategoria);
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Producto pcategoria = new Producto
+                        {
+                            Id = (int)reader["Id"],
+                            Descripcion = (string)reader["Descripcion"],
+                            IdCategoria = (int)reader["IdCategoria"],
+                            Stock = (int)reader["Stock"],
+                            Precio = (decimal)reader["Precio"]
+                        };
+                        pcategorias.Add(pcategoria);
+                    }
+                }
+
+                return pcategorias;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ SistemaVentasSoap/DataAcess/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,19 @@
+﻿using SistemaVentasSoap.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVentasSoap.DataAcess.Interfaces
+{
+    internal interface ICategoriaRepository
+    {
+        //Metodo para listar categoria
+        List<Categoria> GetAll();
+        //Metodo para gregar categoria
+        string AgregarCategoria(Categoria categoria);
+        //Metodo para listar productos por categoria
+        List<Producto> ListarPorCategorias(int IdCategoria);
+    }
+}
